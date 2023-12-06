ada23660c594a30b3bcfa75767d3aed399e6a449
Victor de Moura Netto <victorhmnetto@gmail.com>
2023-12-06 01:25:51 +0100

    feat: add SensorInformation to MainActivity

--- app/src/main/java/xyz/vicnetto/amio_project/MainActivity.java
+++ app/src/main/java/xyz/vicnetto/amio_project/MainActivity.java
@@ -1,8 +1,11 @@
 package xyz.vicnetto.amio_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import java.io.IOException;
+
+import xyz.vicnetto.amio_project.sensor.SensorRequest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SensorRequest sensorRequest = new SensorRequest();
+        try {
+            sensorRequest.printSensorInformation();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
