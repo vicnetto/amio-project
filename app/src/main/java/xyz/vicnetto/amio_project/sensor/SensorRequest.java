ff77fdd380b9b657757a1f0e0ffa685544ce570f
Victor de Moura Netto <victorhmnetto@gmail.com>
2023-12-12 22:14:02 +0100

    refactor: turn SensorRequest into a static class

--- app/src/main/java/xyz/vicnetto/amio_project/sensor/SensorRequest.java
+++ app/src/main/java/xyz/vicnetto/amio_project/sensor/SensorRequest.java
@@ -1,8 +1,5 @@
 package xyz.vicnetto.amio_project.sensor;
 
-import android.util.Log;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,34 +11,19 @@ import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import xyz.vicnetto.amio_project.MainActivity;
 import xyz.vicnetto.amio_project.ui.MainView;
 
 public class SensorRequest {
 
-    private final MainActivity mainActivity;
-
     public static final String BASE_URL = "http://iotlab.telecomnancy.eu:8080/iotlab/rest/data/1/light1/last";
-
-    /**
-     * Constructor of SensorRequest
-     *
-     * @param mainActivity -> The context is needed to use the Toast library, to make popup's.
-     */
-    public SensorRequest(MainActivity mainActivity) {
-        this.mainActivity = mainActivity;
-    }
 
     /**
      * To get the sensor information, we should call this static function. To treat the result:
      * call.enqueue(new Callback() {...})
-     *
      * In this case, we will have two functions inside the Callback. On is in case of success and the
      * other one in case it fails.
-     *
-     * @return
      */
-    public static Call getSensorInformation() {
+    private static Call getSensorInformation() {
         Request request = new Request.Builder()
                 .url(BASE_URL)
                 .build();
@@ -50,29 +32,24 @@ public class SensorRequest {
     }
 
     /**
-     * This function make a new request to the URL, getting the status of the sensors.
-     * Successful request: UI will be updated.
-     * Failure request: A popup will be shown saying about the error.
-     *
-     * @param mainView -> Contains all elements of the UI.
+     * This function make a new request to the URL, getting the status of the sensors. In case of
+     * success, it updates the UI and all the singletons (SensorDataHolder and MainView).
      */
-    public void printSensorInformation(MainView mainView) {
+    public static void getAndUpdateSensorInformation() {
         // After the call specification, it will make the call.
         Call call = getSensorInformation();
-        Toast.makeText(mainActivity, "Request sent!", Toast.LENGTH_SHORT).show();
 
         call.enqueue(new Callback() {
             public void onResponse(@NonNull Call call, @NonNull Response response)
                     throws IOException {
                 if (response.isSuccessful() && response.body() != null) {
                     // Convert JSON data to class and update the UI.
-                    convertDataAndUpdateUI(mainView, response);
+                    treatJSONResponseAndUpdateUI(response);
                 }
             }
 
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                // Failure on the process, informing the user using Toast.
-                failedToSendRequest();
+                // No action.
             }
         });
     }
@@ -81,29 +58,16 @@ public class SensorRequest {
      * In case the request was a success, this function converts the JSON to a class and then
      * updates the UI.
      *
-     * @param mainView -> The main view with all the UI components.
      * @param response -> The JSON response from the request.
      * @throws IOException -> ObjectMapper requests a IOException.
      */
-    private void convertDataAndUpdateUI(MainView mainView, Response response) throws IOException {
+    private static void treatJSONResponseAndUpdateUI(Response response) throws IOException {
         // If response body is not null, it will map the JSON to a class.
         ObjectMapper objectMapper = new ObjectMapper();
-        JSONSensorData JSONSensorData = objectMapper.readValue(response.body().string(), JSONSensorData.class);
+        SensorDataHolder sensorDataHolder = objectMapper.readValue(response.body().string(), SensorDataHolder.class);
+        SensorDataHolder.updateSingleton(sensorDataHolder);
 
         // After converting from JSON to a class, now the UI can be updated.
-        mainView.updateViewAccordingToData(JSONSensorData);
-    }
-
-    /**
-     * In case it wasn't possible to make the request, a error message will be shown with Toast.
-     */
-    private void failedToSendRequest() {
-        mainActivity.runOnUiThread(new Runnable() {
-            public void run() {
-                Toast.makeText(mainActivity,
-                        "Request failed! Are you connected to TN EDUROAM or VPN?"
-                        , Toast.LENGTH_SHORT).show();
-            }
-        });
+        MainView.getInstance().updateViewAccordingToData(sensorDataHolder);
     }
 }
