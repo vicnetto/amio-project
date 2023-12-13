554c3eebf6c2e1a530355d0ac89a8d963a4ba12b
TANG Wenjia <wenjia.tang@telecomnancy.eu>
2023-12-13 01:08:32 +0100

    feat: new function to send mail

---
+++ app/src/main/java/xyz/vicnetto/amio_project/mail/Utils.java
@@ -0,0 +1,8 @@
+package xyz.vicnetto.amio_project.mail;
+
+public class Utils {
+    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
+
+    public static final  String EMAIL = "amioinfotn@gmail.com";
+    public static final String PASSWORD = "yrftazuhdfegiqfi ";
+}
